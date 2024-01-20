a4e0e6b7303f7c191eed96ef33d0a8e7175c536c
gololobov <gololobov.serhiy@gmail.com>
2024-01-20 13:17:04 +0200

    20.01.2024

--- Lesson 10.py
+++ Lesson 10.py
@@ -19,3 +19,15 @@ for k in range(3):
         for j in range(2*i+1+2*k):
             print('*', end='')
         print()'''
+
+c = 0
+for h in range(24):
+    for m in range(60):
+        h1 = h // 10
+        h2 = h % 10
+        m1 = m // 10
+        m2 = m % 10
+        if h1 == m2 and h2 == m1:
+            print(f"{h1}{h2}:{m1}{m2}")
+            c += 1
+print(c)
